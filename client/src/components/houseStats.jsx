5b2f3811efec12688011908eff201b6e355a344f
Sadie Gill <thesadie@gmail.com>
2019-02-03 12:02:14 -0800

    Adds the little dots between house stats

--- client/src/components/houseStats.jsx
+++ client/src/components/houseStats.jsx
@@ -3,9 +3,19 @@ import React from 'react';
 function HouseStats() {
   return (
     <div id="houseStats">
-      <div>4 beds</div>
-      <div>3 baths</div>
-      <div>2,848 sqft</div>
+      <div className='stat'>4 beds</div>
+      <span>
+        <svg height="10" width="10">
+          <rect x="3" y="3" width="3" height="3" fill="#000" />
+        </svg>
+      </span>
+      <div className='stat'>3 baths</div>
+      <span>
+        <svg height="10" width="10">
+          <rect x="3" y="3" width="3" height="3" fill="#000" />
+        </svg>
+      </span>
+      <div className='stat'>2,848 sqft</div>
     </div>
   )
 }
