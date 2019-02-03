e088271ffd9a34014d9f748048b0198d35e5fff1
Sadie Gill <thesadie@gmail.com>
2019-02-03 09:54:03 -0800

    Adds house stats component

---
+++ client/src/components/houseStats.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function HouseStats() {
+  return (
+    <div>
+      <div>4 beds</div>
+      <div>3 baths</div>
+      <div>2,848 sqft</div>
+    </div>
+  )
+}
+
+export default HouseStats;
