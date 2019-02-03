909bfe1fc30a20e0fba8e56860d875f30005bf6d
Sadie Gill <thesadie@gmail.com>
2019-02-03 10:00:20 -0800

    Adds house description

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -2,12 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Address from './address.jsx';
 import HouseStats from './houseStats.jsx';
+import Description from './description.jsx';
 
 class App extends React.Component {
   constructor(props) {
     super(props)
 
-    this.state = { house: { street: '2091 Califronia St.', city: 'Berkeley', state: 'CA', zipcode: '94703' } }
+    this.state = {
+      house: {
+        street: '2091 Califronia St.',
+        city: 'Berkeley', state: 'CA', zipcode: '94703',
+        description: "Est nostrum rerum voluptatem quas voluptas sunt est qui error. Repellat sed consequatur harum eligendi. Dolor accusamus facilis corrupti illo.\n \rNam repellat dolorem et rem delectus vel ut. Alias suscipit cumque asperiores itaque voluptas et fuga. Blanditiis earum assumenda atque cupiditate veritatis. At minus corrupti."
+      }
+    }
   }
 
   render() {
@@ -16,8 +23,10 @@ class App extends React.Component {
         <div id='houseDescription'>
           <Address house={this.state.house} />
           <HouseStats />
+          <Description house={this.state.house}/>
         </div>
-        <div id='housePrice'></div>
+        <div id='housePrice'>
+        </div>
       </div>
     )
   }
