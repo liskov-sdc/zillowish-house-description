80d5eeafbe46db6638e16cf62878966897bf6fbb
Sadie Gill <thesadie@gmail.com>
2019-02-05 20:56:18 -0800

    Tests for database

--- test/database-test.js
+++ test/database-test.js
@@ -1,5 +1,8 @@
+'use strict';
+
 const Sequelize = require('sequelize');
 const password = require('./../config.js');
+const expect = require('chai').expect;
 
 const sequelize = new Sequelize('zillow', 'postgres', password, {
   host: 'localhost',
@@ -14,9 +17,57 @@ const sequelize = new Sequelize('zillow', 'postgres', password, {
   },
 });
 
-test('There are 100 records in the house table', () => {
-  return sequelize.query('select count(*) from houses;', { type: sequelize.QueryTypes.SELECT})
-    .then(count => {
-      expect(count[0].count).toBe(100);
-    })
-})
+describe('Database Seeding', () => {
+
+  var house;
+
+  before((done) => {
+    sequelize.query('select * from houses limit 1;', { type: sequelize.QueryTypes.SELECT })
+      .then(data => {
+        house = data;
+        done()
+      })
+  });
+
+  it('Should have 100 records in the house table', () => {
+    sequelize.query('select count(*) from houses;', { type: sequelize.QueryTypes.SELECT })
+      .then(count => {
+        expect(count[0].count).to.equal('100');
+      })
+      .done()
+  });
+
+  it('Should have records with correct columns in the house table', () => {
+    expect(Object.keys(house[0])).to.deep.equal(['id', 'street', 'city', 'state', 'zipcode', 'description', 'price']);
+  });
+
+  it('Should have a numeric value price', () => {
+    expect(house[0].price).to.be.a('number');
+  });
+
+  it('Should have a numeric id', () => {
+    expect(house[0].id).to.be.a('number');
+  });
+
+  it('Should have a string value for street', () => {
+    expect(house[0].street).to.be.a('string');
+  });
+
+  it('Should have a string value for city', () => {
+    expect(house[0].city).to.be.a('string');
+  });
+
+  it('Should have a string value for state', () => {
+    expect(house[0].state).to.be.a('string');
+  });
+
+  it('Should have a string value for zipcode', () => {
+    expect(house[0].zipcode).to.be.a('string');
+  });
+
+  it('Should have a string value for description', () => {
+    expect(house[0].description).to.be.a('string');
+  });
+
+});
+
