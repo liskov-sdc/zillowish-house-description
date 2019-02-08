350fc1ce7be691d5ff6d48cd4b7b4e418bc36972
Sadie Gill <thesadie@gmail.com>
2019-02-07 18:32:10 -0800

    adds house id param to url api

--- server/index.js
+++ server/index.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const port = 3001
 const House = require('./../database/index.js')
 
 app.use(express.static(__dirname + '/./../client/dist'))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
-app.get('/houses', (req, res) => {
-  House.findAll().then(data => {
+app.get('/houses/:id', (req, res) => {
+  House.findAll(
+    {where: {id: req.params.id}}
+  ).then(data => {
     res.send(data)
   });
 });
