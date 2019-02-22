14397f5a34226b5fe1af56d60ef069b2e22f77e9
Sadie Gill <thesadie@gmail.com>
2019-02-22 10:57:53 -0800

    Refactors app to use new price service

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -11,7 +11,6 @@ class App extends React.Component {
     super(props)
 
     this.state = {
-      id: 99,
       house: {},
       beds: 4,
       baths: 3,
@@ -33,33 +32,17 @@ class App extends React.Component {
           description: house.description
         }
       });
-    })
-  }
 
-  componentDidUpdate() {
-    let id = window.location.pathname.split('/')[1]; //revise
-    $.get(`http://localhost:3001/houses/${id}`, (data) => {
-      let house = data[0];
-      this.setState({
-        house: {
-          street: house.street,
-          city: house.city,
-          state: house.state,
-          zipcode: house.zipcode,
-          description: house.description
-        }
+      $.get(`http://localhost:3001/prices/${id}`, (data) => {
+        this.setState({price: data[0].price})
       });
-    })
-  }
 
-  onIdChange(event) {
-    this.setState({id: event.target.value});
+    });
   }
 
   render() {
     return (
       <div id='houseSummary'>
-      <input onChange={this.onIdChange.bind(this)}></input>
         <div id='addressPrice'>
           <div id='addressStats'>
             <Address house={this.state.house} />
--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const port = 3001
-const House = require('./../database/index.js')
+const db = require('./../database/index.js')
 const cors = require('cors')
 
 app.use(express.static(__dirname + '/./../client/dist'))
@@ -14,7 +14,19 @@ app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 
 app.get('/houses/:id', (req, res) => {
-  House.findAll(
+  db.House.findAll(
+    {where: {id: req.params.id}}
+  ).then(data => {
+    res.status(200).send(data);
+  })
+  .catch(err => {
+    console.error(err);
+    res.status(404).send()
+  });
+});
+
+app.get('/prices/:id', (req, res) => {
+  db.Price.findAll(
     {where: {id: req.params.id}}
   ).then(data => {
     res.status(200).send(data);
